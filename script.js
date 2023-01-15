fa1efdb78b5c19c8f9bbbf26aec9d991b928d869
karolinelucas <karolinecristinalucas7@gmail.com>
2023-01-15 20:08:26 -0300

    challenge

---
+++ script.js
@@ -0,0 +1,34 @@
+ function criptografar(){
+    var texto = document.getElementById('inputText').value.toLowerCase();
+   
+    var txtCripto = texto.replace(/e/ig, 'enter').replace(/o/ig, 'ober').replace(/i/ig, 'ines').replace(/a/ig, 'ai').replace(/u/ig, 'ufat'); 
+
+    document.getElementById('imgder').style.display = 'none';
+    document.getElementById('inputText').style.display = 'none';
+    document.getElementById('texto2').innerHTML = txtCripto;
+    document.getElementById('copiar').style.display = 'show';
+    document.getElementById('copiar').style.display = 'inherit';
+
+}
+
+function descriptografar(){
+    var texto = document.getElementById('inputText').value.toLowerCase();
+   
+    var txtCripto = texto.replace(/enter/ig, 'e').replace(/ober/ig, 'o').replace(/ines/ig, 'i').replace(/ai/ig, 'a').replace(/ufat/ig, 'u'); 
+
+    document.getElementById('imgder').style.display = 'none';
+    document.getElementById('inputText').style.display = 'none';
+    document.getElementById('texto2').innerHTML = txtCripto;
+    document.getElementById('copiar').style.display = 'show';
+    document.getElementById('copiar').style.display = 'inherit';
+
+}
+
+function copiar(){
+    var conteudo = document.querySelector('#texto2');
+    conteudo.select();
+    document.execCommand('copy')
+
+    alert('Texto copiado!')
+}
+
